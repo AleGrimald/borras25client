e08f43d3ee41be5771c34fecc16454f54705cf86
AleGrimaldi <grimaldialejandro5@gmail.com>
2025-01-23 01:39:57 -0300

    Se agrego funcionalidad al formulario de alumnos

--- src/component/Admin/index.jsx
+++ src/component/Admin/index.jsx
@@ -4,11 +4,36 @@ import { useEffect, useState } from 'react';
 const Admin =(props)=>{
     const {manejoLogin} = props;
     const [usuario, setUsuarios] = useState([]);
+    const [datosAlumno, setDatosAlumno] = useState({});
+
     const [btnAlumno, setBtnAlumno] = useState(false);
     const [btnRutina, setBtnRutina] = useState(false);
     const [btnEjercicio, setBtnEjercicio] = useState(false);
+
+    const [apellido, setApellido] = useState("");
+    const [nombre, setNombre] = useState("");
+    const [dni, setDni] = useState("");
+    const [edad, setEdad] = useState("");
+    const [telefono, setTelefono] = useState("");
+    const [email, setEmail] = useState("");
+    const [pais, setPais] = useState("");
+    const [provincia, setProvincia] = useState("");
+    const [departamento, setDepartamento] = useState("");
+    const [localidad, setLocalidad] = useState("");
+    const [calle, setCalle] = useState("");
+    const [numero, setNumero] = useState("");
+    const [piso, setPiso] = useState("");
+    const [dpto, setDpto] = useState("");
+    const [usuarioInp, setUsuarioInp] = useState("");
+    const [contraseña, setContraseña] = useState("");
+    const [confContraseña, setConfContraseña] = useState("");
+    const [opcionPlan, setOpcionPlan] = useState("0");
+    const [fechaI, setFechaI] = useState("");
+    const [fechaF, setFechaF] = useState("");
+
     const [manejoListarAlumno, setManejoListarAlumno] = useState(false);
     const [manejoAgregarAlumno, setManejoAgregarAlumno] = useState(false);
+
 
     useEffect(() => {
         const urlP = 'https://borras25server.vercel.app/admin';
@@ -25,9 +50,82 @@ const Admin =(props)=>{
         .catch(error => console.error('Error fetching data:', error));
     });
 
+    useEffect(() => {
+        if (fechaI) {
+            const fechaInicio = new Date(fechaI);
+            const fechaFin = new Date(fechaInicio);
+            fechaFin.setDate(fechaFin.getDate() + 30);
+            setFechaF(fechaFin.toISOString().split('T')[0]); // Formatear la fecha a 'YYYY-MM-DD'
+        }
+    }, [fechaI]);
+
+    const manejoFormulario = async (e)=>{
+        e.preventDefault();
+        let datos={};
+
+        if(contraseña===confContraseña){
+            switch(opcionPlan){
+                case "Basico": setOpcionPlan("1"); break;
+                case "Competitivo": setOpcionPlan("2"); break;
+                case "Profesional": setOpcionPlan("3"); break;
+            }
+
+            datos = {
+                ape: apellido,
+                nom: nombre,
+                dni: dni,
+                ed: edad,
+                tel: telefono,
+                mail: email,
+                pais: pais,
+                prov: provincia,
+                dep: departamento,
+                loc: localidad,
+                calle: calle,
+                num: numero,
+                piso: piso,
+                dpto: dpto,
+                usu: usuarioInp,
+                pass: contraseña,
+                plan: opcionPlan,
+                fechaInicio: fechaI,
+                fechaFin: fechaF
+            }
+            setDatosAlumno(datos);
+
+            try{
+                const response = await fetch('https://borras25server.vercel.app/agregar_alumno',{
+                    method: 'POST',
+                    headers:{
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify(datosAlumno)
+                });
+
+                if(response.ok){
+                    console.log("¡Alumno agregado exitosamente!")
+                }else{
+                    console.log("Error al enviar los datos.")
+                }
+            }
+            catch(error){
+                console.log(`Error al enviar los datos: ${error}`)
+            }
+        }
+    }
+
     const reiniciarEstados=()=>{
         setManejoAgregarAlumno(false);
         setManejoListarAlumno(false);
+    }
+
+    const manejoListadoA =()=>{
+        reiniciarEstados();
+        setManejoListarAlumno(!manejoListarAlumno);
+    }
+    const manejoAgregarA = ()=>{
+        reiniciarEstados();
+        setManejoAgregarAlumno(!manejoAgregarAlumno);
     }
 
     return<section className='contenedor_admin'>
@@ -51,10 +149,8 @@ const Admin =(props)=>{
                     {
                         btnAlumno?
                             <ul className='lista_admin'>
-                                <li onClick={()=>{setManejoListarAlumno(!manejoListarAlumno)}} className='lista_admin_elemento'>Listar Alumnos</li>
-                                <li onClick={()=>{setManejoAgregarAlumno(!manejoAgregarAlumno)}} className='lista_admin_elemento'>Agregar Alumno</li>
-                                <li onClick={reiniciarEstados} className='lista_admin_elemento'>Modificar Alumno</li>
-                                <li onClick={reiniciarEstados} className='lista_admin_elemento'>Eliminar Alumno</li>
+                                <li onClick={manejoListadoA} className='lista_admin_elemento'>Listar Alumnos</li>
+                                <li onClick={manejoAgregarA} className='lista_admin_elemento'>Agregar Alumno</li>
                             </ul>
                         :<></>
                     }
@@ -65,10 +161,8 @@ const Admin =(props)=>{
                     {
                         btnRutina?
                             <ul className='lista_admin'>
-                                <li onClick={manejoPanelCentral} className='lista_admin_elemento'>Listar Rutina</li>
-                                <li onClick={manejoPanelCentral} className='lista_admin_elemento'>Crear Rutina</li>
-                                <li onClick={manejoPanelCentral} className='lista_admin_elemento'>Modificar Rutina</li>
-                                <li onClick={manejoPanelCentral} className='lista_admin_elemento'>Eliminar Rutina</li>
+                                <li className='lista_admin_elemento'>Listar Rutina</li>
+                                <li className='lista_admin_elemento'>Crear Rutina</li>
                             </ul>
                         :<></>
                     }
@@ -79,10 +173,8 @@ const Admin =(props)=>{
                     {
                         btnEjercicio?
                             <ul className='lista_admin'>
-                                <li onClick={manejoPanelCentral} className='lista_admin_elemento'>Listar Ejercicios</li>
-                                <li onClick={manejoPanelCentral} className='lista_admin_elemento'>Crear Ejercicio</li>
-                                <li onClick={manejoPanelCentral} className='lista_admin_elemento'>Modificar Ejercicio</li>
-                                <li onClick={manejoPanelCentral} className='lista_admin_elemento'>Eliminar Ejercicio</li>
+                                <li className='lista_admin_elemento'>Listar Ejercicios</li>
+                                <li className='lista_admin_elemento'>Crear Ejercicio</li>
                             </ul>
                         :<></>
                     }
@@ -105,120 +197,112 @@ const Admin =(props)=>{
                                 <p className='cuenta_p_central'>Entrenamiento: {user.nombre}</p>
                                 <p className='cuenta_p_central'>Estado: {user.estado}</p>
                             </section>
+                            <section className='section_alumno_btn_central'>
+                                <button className='alumno_btn_editar'>Editar</button>
+                                <button className='alumno_btn_borrar'>Borrar</button>
+                            </section>
                         </article>):<></>
                 }
 
                 {
-                    manejoAgregarAlumno?<form action="" className='form'>
+                    manejoAgregarAlumno?
+                    <form onSubmit={manejoFormulario} action="" className='form'>
                         <h2 className='form_h2'>Formulario de Inscripcion</h2>
                         <fieldset className='form_field'>
                             <legend className='form_legend'>Datos Personales</legend>
                             <div className='form_div'>
-                                <label htmlFor="">Apellido *</label>
-                                <input type="text" name="" id="" required/>
+                                <label htmlFor="apellido">Apellido *</label>
+                                <input onChange={(e) => setApellido(e.target.value)} type="text" name="apellido" id="apellido" required/>
                             </div>
                             <div className='form_div'>
-                                <label htmlFor="">Nombre *</label>
-                                <input type="text" name="" id="" required/>
+                                <label htmlFor="nombre">Nombre *</label>
+                                <input onChange={(e) => setNombre(e.target.value)} type="text" name="nombre" id="nombre" required/>
                             </div>
                             <div className='form_div'>
-                                <label htmlFor="">Dni *</label>
-                                <input type="text" name="" id="" required/>
+                                <label htmlFor="dni">Dni *</label>
+                                <input onChange={(e) => setDni(e.target.value)} type="text" name="dni" id="dni" required/>
                             </div>
                             <div className='form_div'>
-                                <label htmlFor="">Edad *</label>
-                                <input type="text" name="" id="" required/>
-                            </div>
-                            
-                            <div className='form_div'>
-                                <label htmlFor="">Telefono *</label>
-                                <input type="text" name="" id="" required/>
+                                <label htmlFor="edad">Edad *</label>
+                                <input onChange={(e) => setEdad(e.target.value)} type="text" name="edad" id="edad" required/>
                             </div>
                             <div className='form_div'>
-                                <label htmlFor="">Email *</label>
-                                <input type="text" name="" id="" required/>
+                                <label htmlFor="telefono">Telefono *</label>
+                                <input onChange={(e) => setTelefono(e.target.value)} type="text" name="telefono" id="telefono" required/>
                             </div>
-
+                            <div className='form_div'>
+                                <label htmlFor="email">Email *</label>
+                                <input onChange={(e) => setEmail(e.target.value)} type="text" name="email" id="email" required/>
+                            </div>
                             <fieldset className='form_field'>
                                 <legend className='form_legend'>Direccion</legend>
                                 <div className='form_div'>
-                                    <label htmlFor="">Pais *</label>
-                                    <input type="text" name="" id="" required/>
+                                    <label htmlFor="pais">Pais *</label>
+                                    <input onChange={(e) => setPais(e.target.value)} type="text" name="pais" id="pais" required/>
                                 </div>
                                 <div className='form_div'>
-                                    <label htmlFor="">Provincia *</label>
-                                    <input type="text" name="" id="" required/>
+                                    <label htmlFor="provincia">Provincia *</label>
+                                    <input onChange={(e) => setProvincia(e.target.value)} type="text" name="provincia" id="provincia" required/>
                                 </div>
                                 <div className='form_div'>
-                                    <label htmlFor="">Departamento</label>
-                                    <input type="text" name="" id="" />
+                                    <label htmlFor="departamento">Departamento</label>
+                                    <input onChange={(e) => setDepartamento(e.target.value)} type="text" name="departamento" id="departamento" />
                                 </div>
                                 <div className='form_div'>
-                                    <label htmlFor="">Localidad</label>
-                                    <input type="text" name="" id="" />
+                                    <label htmlFor="localidad">Localidad</label>
+                                    <input onChange={(e) => setLocalidad(e.target.value)} type="text" name="localidad" id="localidad" />
                                 </div>
                                 <div className='form_div'>
-                                    <label htmlFor="">Calle *</label>
-                                    <input type="text" name="" id="" required/>
+                                    <label htmlFor="calle">Calle *</label>
+                                    <input onChange={(e) => setCalle(e.target.value)} type="text" name="calle" id="calle" required/>
                                 </div>
                                 <div className='form_div'>
-                                    <label htmlFor="">Numero *</label>
-                                    <input type="number" name="" id="" required/>
+                                    <label htmlFor="numero">Numero *</label>
+                                    <input onChange={(e) => setNumero(e.target.value)} type="number" name="numero" id="numero+" required/>
                                 </div>
                                 <div className='form_div'>
-                                    <label htmlFor="">Piso</label>
-                                    <input type="text" name="" id="" />
+                                    <label htmlFor="piso">Piso</label>
+                                    <input onChange={(e) => setPiso(e.target.value)} type="text" name="piso" id="piso" />
                                 </div>
                                 <div className='form_div'>
-                                    <label htmlFor="">Dpto</label>
-                                    <input type="text" name="" id="" />
+                                    <label htmlFor="dpto">Dpto</label>
+                                    <input onChange={(e) => setDpto(e.target.value)} type="text" name="dpto" id="dpto" />
                                 </div>
-                                
                             </fieldset>
                         </fieldset>
 
                         <fieldset className='form_field'>
                             <legend className='form_legend'>Datos de Usuario</legend>
                             <div className='form_div'>
-                                <label htmlFor="">Usuario *</label>
-                                <input type="text" name="" id="" required/>
+                                <label htmlFor="usuario">Usuario *</label>
+                                <input onChange={(e) => setUsuarioInp(e.target.value)} type="text" name="usuario" id="usuario" required/>
                             </div>
                             <div className='form_div'>
-                                <label htmlFor="">Contraseña *</label>
-                                <input type="password" name="" id="" required/>
+                                <label htmlFor="contraseña">Contraseña *</label>
+                                <input onChange={(e) => setContraseña(e.target.value)} type="password" name="contraseña" id="contraseña" required/>
                             </div>
                             <div className='form_div'>
-                                <label htmlFor="">Confirmar Contraseña *</label>
-                                <input type="text" name="" id="" required/>
+                                <label htmlFor="confContraseña">Confirmar Contraseña *</label>
+                                <input onChange={(e) => setConfContraseña(e.target.value)} type="text" name="confContraseña" id="confContraseña" required/>
                             </div>
-                            
                             <div className='form_div'>
-                                <label htmlFor="">Plan de Entrenamiento *</label>
-                                <select name="" id="" required>
-                                    <option value="" disabled selected>Seleccione una opcion</option>
-                                    <option value="">Basico</option>
-                                    <option value="">Intermedio</option>
-                                    <option value="">Profecional</option>
-                                    <option value="">Nivel Asiatico</option>
+                                <label htmlFor="planE">Plan de Entrenamiento *</label>
+                                <select value={opcionPlan} onChange={(e)=>{setOpcionPlan(e.target.value)}} name="planE" id="planE" required>
+                                    <option value="0">Selecciona una opcion</option>
+                                    <option value="1">Basico</option>
+                                    <option value="2">Competitivo</option>
+                                    <option value="3">Profesional</option>
                                 </select>
                             </div>
-                            
                         </fieldset>
-                        
+
                         <fieldset className='form_field'>
                             <legend className='form_legend'>Fecha de Inicio - Fin</legend>
-
                             <div className='form_div'>
-                                <label htmlFor="">Fecha de Inicio *</label>
-                                <input type="date" name="" id="" required/>
-                            </div>
-
-                            <div className='form_div'>
-                                <label htmlFor="">Fecha de Fin *</label>
-                                <input type="date" name="" id="" required/>
+                                <label htmlFor="fechaI">Fecha de Inicio *</label>
+                                <input onChange={(e) => setFechaI(e.target.value)} type="date" name="fechaI" id="fechaI" required/>
                             </div>
                         </fieldset>
-
                     </form>:<></>
                 }
             </article>
